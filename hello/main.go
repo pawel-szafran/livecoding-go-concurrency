d7b6ffd74eb0dbf0436307dacae908ed65a29c01
Pawel Szafran <pawel.szafran@gmail.com>
2015-07-09 22:36:15 -0400

    helloWorker

--- hello/main.go
+++ hello/main.go
@@ -7,13 +7,15 @@ import (
 
 func main() {
 	requests := make(chan chan string)
-	go func() {
-		response := <-requests
-		response <- hello()
-	}()
+	go helloWorker(requests)
 	response := make(chan string)
 	requests <- response
 	fmt.Println(<-response)
+}
+
+func helloWorker(requests chan chan string) {
+	response := <-requests
+	response <- hello()
 }
 
 func hello() string {
