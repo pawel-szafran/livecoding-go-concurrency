7976dfb6a4e3190d0a8dd51dadac94cbe1d20ecf
Pawel Szafran <pawel.szafran@gmail.com>
2015-07-10 00:16:49 -0400

    Clients know less

--- hello/main.go
+++ hello/main.go
@@ -8,11 +8,36 @@ import (
 
 func main() {
 	start := time.Now()
-	requests := helloBroker()
-	lotsOfHelloClients(requests)
-	close(requests)
+	broker := startNewHelloBroker()
+	lotsOfHelloClients(broker)
+	broker.Stop()
 	elapsed := time.Since(start)
 	fmt.Println("Elapsed:", elapsed)
+}
+
+type HelloProvider interface {
+	RequestHello(name string) <-chan string
+}
+
+func lotsOfHelloClients(helloProvider HelloProvider) {
+	var done sync.WaitGroup
+	for i := 0; i < 10000; i++ {
+		done.Add(1)
+		go func(id int) {
+			defer done.Done()
+			helloClient(id, helloProvider)
+		}(i)
+	}
+	done.Wait()
+}
+
+func helloClient(id int, helloProvider HelloProvider) {
+	name := fmt.Sprint("Client-", id)
+	fmt.Println(<-helloProvider.RequestHello(name))
+}
+
+type HelloBroker struct {
+	requests chan helloRequest
 }
 
 type helloRequest struct {
@@ -20,36 +45,26 @@ type helloRequest struct {
 	response chan string
 }
 
-func newHelloRequest(name string) helloRequest {
-	return helloRequest{name, make(chan string)}
+func startNewHelloBroker() *HelloBroker {
+	broker := &HelloBroker{make(chan helloRequest)}
+	go broker.start()
+	return broker
 }
 
-func lotsOfHelloClients(requests chan<- helloRequest) {
-	var done sync.WaitGroup
-	for i := 0; i < 10000; i++ {
-		done.Add(1)
-		go func(id int) {
-			defer done.Done()
-			helloClient(id, requests)
-		}(i)
+func (broker *HelloBroker) start() {
+	for i := 0; i < 1000; i++ {
+		go helloWorker(broker.requests)
 	}
-	done.Wait()
 }
 
-func helloClient(id int, requests chan<- helloRequest) {
-	request := newHelloRequest(fmt.Sprint("Client-", id))
-	requests <- request
-	fmt.Println(<-request.response)
+func (broker *HelloBroker) Stop() {
+	close(broker.requests)
 }
 
-func helloBroker() chan<- helloRequest {
-	requests := make(chan helloRequest)
-	go func() {
-		for i := 0; i < 1000; i++ {
-			go helloWorker(requests)
-		}
-	}()
-	return requests
+func (broker *HelloBroker) RequestHello(name string) <-chan string {
+	request := helloRequest{name, make(chan string)}
+	broker.requests <- request
+	return request.response
 }
 
 func helloWorker(requests <-chan helloRequest) {
