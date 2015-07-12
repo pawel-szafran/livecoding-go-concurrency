cf011a81f39029da44b0dbba7d816bc1e5f7306c
Pawel Szafran <pawel.szafran@gmail.com>
2015-07-12 19:07:38 -0400

    Cleanup

--- search/aggregator.go
+++ search/aggregator.go
@@ -9,21 +9,29 @@ type Aggregator struct {
 
 func (a *Aggregator) Search(query Query) Results {
 	resultChan := make(chan typedResult)
+	a.searchAll(query, resultChan)
+	return a.collectResults(resultChan)
+}
+
+type typedResult struct {
+	searchType SearchType
+	result     Result
+}
+
+func (a *Aggregator) searchAll(query Query, resultChan chan typedResult) {
 	for searchType, search := range a.Searches {
 		searchType, search := searchType, search
 		go func() {
 			resultChan <- typedResult{searchType, search(query)}
 		}()
 	}
+}
+
+func (a *Aggregator) collectResults(resultChan chan typedResult) Results {
 	results := Results{}
 	for i := 0; i < len(a.Searches); i++ {
 		result := <-resultChan
 		results[result.searchType] = result.result
 	}
 	return results
-}
-
-type typedResult struct {
-	searchType SearchType
-	result     Result
 }
