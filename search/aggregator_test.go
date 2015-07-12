efc3800289a45de20589ebcff1742728eb0fcccb
Pawel Szafran <pawel.szafran@gmail.com>
2015-07-11 22:06:38 -0400

    Ginkgo bootstrap and generate - no buildable Go source

---
+++ search/aggregator_test.go
@@ -0,0 +1,12 @@
+package search_test
+
+import (
+	. "github.com/pawel-szafran/livecoding-go-concurrency/search"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Aggregator", func() {
+
+})
