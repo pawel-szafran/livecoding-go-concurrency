b587eb3e19bf670729922ba5aba574e8e1e10aff
Pawel Szafran <pawel.szafran@gmail.com>
2015-07-12 18:55:58 -0400

    Behavior: it aggregates concurrently - test fails

--- search/aggregator_test.go
+++ search/aggregator_test.go
@@ -2,6 +2,7 @@ package search_test
 
 import (
 	"fmt"
+	"time"
 
 	. "github.com/pawel-szafran/livecoding-go-concurrency/search"
 
@@ -23,10 +24,27 @@ var _ = Describe("Aggregator", func() {
 		}))
 	})
 
+	It("aggregates results concurrently", func() {
+		aggregator := Aggregator{Searches{
+			"Photos": fakeLongSearch("Photos", time.Millisecond),
+			"Videos": fakeLongSearch("Videos", time.Millisecond),
+		}}
+		start := time.Now()
+		aggregator.Search("golang")
+		Expect(time.Since(start)).To(BeNumerically("<", 2*time.Millisecond))
+	})
+
 })
 
 func fakeSearch(name string) Search {
 	return func(query Query) Result {
 		return Result(fmt.Sprint(name, " result for ", query))
+	}
+}
+
+func fakeLongSearch(name string, duration time.Duration) Search {
+	return func(query Query) Result {
+		time.Sleep(duration)
+		return fakeSearch(name)(query)
 	}
 }
